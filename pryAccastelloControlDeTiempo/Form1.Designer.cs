e2b0ddaf95511ddda3e43fa6524dfc4d40c0dc03
andresaccastello <andres.solte2@gmail.com>
2025-05-12 13:13:45 -0300

    Agregar archivos de proyecto.

---
+++ pryAccastelloControlDeTiempo/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace pryAccastelloControlDeTiempo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTemporizador = new System.Windows.Forms.Button();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTemporizador
+            // 
+            this.btnTemporizador.Location = new System.Drawing.Point(52, 19);
+            this.btnTemporizador.Name = "btnTemporizador";
+            this.btnTemporizador.Size = new System.Drawing.Size(75, 23);
+            this.btnTemporizador.TabIndex = 0;
+            this.btnTemporizador.Text = "Trabajo";
+            this.btnTemporizador.UseVisualStyleBackColor = true;
+            this.btnTemporizador.Click += new System.EventHandler(this.btnTemporizador_Click);
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(100, 84);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(35, 13);
+            this.lblTiempo.TabIndex = 1;
+            this.lblTiempo.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tiempo:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTemporizador);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblTiempo);
+            this.groupBox1.Location = new System.Drawing.Point(108, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de tiempo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 259);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTemporizador;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ pryAccastelloControlDeTiempo/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace pryAccastelloControlDeTiempo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            lblTiempo.Text = "0.00 s";
+        }
+
+        Stopwatch cronometro = new Stopwatch();
+        bool temporizadorCorriendo = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTemporizador_Click(object sender, EventArgs e)
+        {
+            if (!temporizadorCorriendo)
+            {
+                cronometro.Restart();
+                temporizadorCorriendo = true;
+                btnTemporizador.Text = "Detener";
+                lblTiempo.Text = "0.00 s";
+            }
+            else
+            {
+                cronometro.Stop();
+                temporizadorCorriendo = false;
+                btnTemporizador.Text = "Iniciar";
+                lblTiempo.Text = $"{cronometro.Elapsed.TotalSeconds:F2} s";
+            }
+        }
+    }
+}
